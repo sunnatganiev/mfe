c64a3ca9a600548550844212a3968c3adc339b94
Sunnatullo Ganiev <sunnatulloganiev@Sunnatullos-MacBook-Pro.local>
2024-03-01 14:39:51 +0000

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { mount } from 'marketing/MarketingApp';
+
+// console.log(mount);
+
+import MarketingApp from '../components/MarketingApp';
+
+function App() {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+}
+
+export default App;
